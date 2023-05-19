5a872e4c50a68c00ab22b8f88c90cc47c97cd494
Jáchym Solecký <jachym.solecky@gendigital.com>
2023-05-19 19:21:02 +0200

    Refactor frame.py to main_graphics.py

--- src/graphics/frame.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.lang.builder import Builder
-
-Builder.load_file('graphics/frame.kv')
-
-class TrainYourModelGame(TabbedPanel):
-    pass
--- src/main_graphics.py
+++ src/main_graphics.py
@@ -1,9 +1,15 @@
-from graphics.frame import TrainYourModelGame
 from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.widget import Widget
+
+kv = Builder.load_file("graphics/frame.kv")
+
+class TrainYourModelGame(Widget):
+    pass
 
 class TrainYourModelApp(App):
     def build(self):
-        return TrainYourModelGame()
+        return kv
 
 if __name__ == "__main__":
     TrainYourModelApp().run()
