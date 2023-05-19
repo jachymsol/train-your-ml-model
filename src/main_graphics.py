ebc22a11d633daf3d5e1b59b2e55dbdd0753d06a
Jáchym Solecký <jachym.solecky@gendigital.com>
2023-05-19 21:00:00 +0200

    Display camera image

--- src/main_graphics.py
+++ src/main_graphics.py
@@ -1,6 +1,11 @@
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.widget import Widget
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+
+from utils.upload_utils import *
+from utils.config import get_config
 
 Builder.load_file("graphics/frame.kv")
 
@@ -8,7 +13,25 @@ class TrainingDataTab(Widget):
     pass
 
 class UploadPictureFrame(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.camera = create_camera()
+        self.is_capturing = True
+        Clock.schedule_interval(self.update_camera, 1.0 / 30)
+
+    def update_camera(self, dt):
+        if self.camera.isOpened() and self.is_capturing:
+            image = capture(self.camera)
+
+            # Convert image into Kivy Texture
+            buf = cv2.flip(image, 0).tobytes()
+            texture = Texture.create(
+                size=(image.shape[1], image.shape[0]),
+                colorfmt='bgr'
+            )
+            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.ids.camera_image.texture = texture
 
 class TrainYourModelGame(Widget):
     pass
