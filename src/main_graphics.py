dcd83cbef3a78e73b6f94b3de1f70d00bce745f0
Jáchym Solecký <jachym.solecky@gendigital.com>
2023-05-19 19:45:32 +0200

    Quick refactor

--- src/main_graphics.py
+++ src/main_graphics.py
@@ -2,14 +2,14 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.widget import Widget
 
-kv = Builder.load_file("graphics/frame.kv")
+Builder.load_file("graphics/frame.kv")
 
 class TrainYourModelGame(Widget):
     pass
 
 class TrainYourModelApp(App):
     def build(self):
-        return kv
+        return TrainYourModelGame()
 
 if __name__ == "__main__":
     TrainYourModelApp().run()
