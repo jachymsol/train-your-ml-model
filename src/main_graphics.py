c57558f4849468f1a24ec9ef668b528c9f80f990
Jáchym Solecký <jachym.solecky@gendigital.com>
2023-05-19 21:47:20 +0200

    Add functionality to upload photo

--- src/main_graphics.py
+++ src/main_graphics.py
@@ -3,35 +3,56 @@ from kivy.lang.builder import Builder
 from kivy.uix.widget import Widget
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.config import Config
 
 from utils.upload_utils import *
 from utils.config import get_config
 
 Builder.load_file("graphics/frame.kv")
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 class TrainingDataTab(Widget):
     pass
 
 class UploadPictureFrame(Widget):
+    is_capturing = BooleanProperty(True)
+    is_selected_first_category = BooleanProperty(True)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
         self.camera = create_camera()
-        self.is_capturing = True
         Clock.schedule_interval(self.update_camera, 1.0 / 30)
 
     def update_camera(self, dt):
         if self.camera.isOpened() and self.is_capturing:
-            image = capture(self.camera)
+            self.image = capture(self.camera)
+            self.update_camera_image()
 
-            # Convert image into Kivy Texture
-            buf = cv2.flip(image, 0).tobytes()
-            texture = Texture.create(
-                size=(image.shape[1], image.shape[0]),
-                colorfmt='bgr'
-            )
-            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            self.ids.camera_image.texture = texture
+    def update_camera_image(self):
+        # Convert image into Kivy Texture
+        buf = cv2.flip(self.image, 0).tobytes()
+        texture = Texture.create(
+            size=(self.image.shape[1], self.image.shape[0]),
+            colorfmt='bgr'
+        )
+        texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.ids.camera_image.texture = texture
+    
+    def capture_image(self):
+        self.is_capturing = False
+
+    def retake_image(self):
+        self.is_capturing = True
+    
+    def save_image(self):
+        image_path = get_image_file_name(
+            get_config('train_folder'),
+            'airplane' if self.is_selected_first_category else 'basketball'
+        )
+        save_image(self.image, image_path)
+        self.is_capturing = True
 
 class TrainYourModelGame(Widget):
     pass
