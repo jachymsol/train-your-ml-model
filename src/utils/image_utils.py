ffb75ba9b8245f156ed314772ad80245ebf12d4b
Jáchym Solecký <jachym.solecky@gendigital.com>
2023-06-28 18:41:06 +0200

    Fix file naming

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -42,10 +42,10 @@ def transform_and_display(image, canvas, transforms):
 
 def get_next_filename(dataset_path, category):
     folder_path = Path.expanduser(Path(dataset_path) / category)
-    current_filenames = [file.name for file in folder_path.glob('train_*.png')]
-    current_filenames.sort()
-    if len(current_filenames) > 0:
-        last_number = int(current_filenames[-1][3:-4])
+    current_filenames = [file.stem for file in folder_path.glob('train_*.png')]
+    file_numbers = [int(filename[-5:]) for filename in current_filenames]
+    if len(file_numbers) > 0:
+        last_number = max(file_numbers)
     else:
         last_number = -1
     new_filename = f"train_{str(last_number+1).zfill(5)}.png"
