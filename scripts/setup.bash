ff19acf5ae9b0da14825a53a27437cdb47fcfe5c
jachymsol <solejabreck@gmail.com>
2023-06-29 23:25:55 +0200

    Make setup script idempotent

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -2,6 +2,8 @@ mkdir -p games
 mkdir -p datasets/train/house
 mkdir -p datasets/train/tree
 
-unzip -q test_images.zip -d ./datasets/
+if [ ! -d datasets/test ]; then
+    unzip -q test_images.zip -d ./datasets/
+fi
 
 sed -i "s:root:$(pwd):g" src/config.yaml
